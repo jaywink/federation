435c3d6d43a81a918d572be2c2b8db0d7783a0f3
Jason Robinson <mail@jasonrobinson.me>
2018-04-08 13:54:54 +0300

    Add NodeInfo2 generator and Django view
    See https://github.com/jaywink/nodeinfo2

--- federation/hostmeta/django/urls.py
+++ federation/hostmeta/django/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from federation.hostmeta.django import rfc3033_webfinger_view
+from federation.hostmeta.django.generators import nodeinfo2_view
 
 urlpatterns = [
     url(r'^.well-known/webfinger$', rfc3033_webfinger_view, name="rfc3033-webfinger"),
+    url(r'^.well-known/x-nodeinfo2$', nodeinfo2_view, name="nodeinfo2"),
 ]
