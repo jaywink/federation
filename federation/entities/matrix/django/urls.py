6c395583610e375a5a02b3b575914ebae43d0e2a
Jason Robinson <mail@jasonrobinson.me>
2020-12-23 23:04:08 +0200

    No need for csrf for Matrix AS transactions

--- federation/entities/matrix/django/urls.py
+++ federation/entities/matrix/django/urls.py
@@ -1,12 +1,14 @@
 # noinspection PyPackageRequirements
 from django.conf.urls import url
+# noinspection PyPackageRequirements
+from django.views.decorators.csrf import csrf_exempt
 
 from federation.entities.matrix.django.views import MatrixASTransactionsView
 
 urlpatterns = [
-    url(
+    csrf_exempt(url(
         regex=r"transactions/(?P<txn_id>[\w-]+)$",
         view=MatrixASTransactionsView.as_view(),
         name="matrix-as-transactions",
-    ),
+    )),
 ]
