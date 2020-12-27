e55f13a11704a498e52942477e3e0b4a7c24997d
Jason Robinson <mail@jasonrobinson.me>
2020-12-27 17:24:27 +0200

    Fix tests

--- federation/tests/entities/diaspora/test_utils.py
+++ federation/tests/entities/diaspora/test_utils.py
@@ -19,7 +19,7 @@ class TestGetBaseAttributes:
         assert set(attrs) == {
             "created_at", "location", "provider_display_name", "public", "raw_content",
             "signature", "base_url", "actor_id", "id", "handle", "guid", "activity", "activity_id",
-            "url",
+            "url", "mxid",
         }
         entity = Profile()
         attrs = get_base_attributes(entity).keys()
