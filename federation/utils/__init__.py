c2759b4768fb812da6946a107604ff4334d34b09
Alain St-Denis <alain@zenfolie.org>
2022-04-23 13:26:06 +0000

    well, it seems setup.py doesn't want third party imports in federation/__init__.py. moved request_cache setup to utils/___init__.py

---
+++ federation/utils/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+from requests_cache import install_cache, RedisCache, SQLiteCache
+
+logger = logging.getLogger("federation")
+
+# try to obtain redis config from django
+cfg = get_configuration()
+if cfg.get('redis'):
+    backend = RedisCache(namespace='fed_cache', **cfg['redis'])
+else:
+    backend = SQLiteCache(db_path='fed_cache')
+install_cache(backend=backend)
+logger.info(f'requests_cache backend set to {type(backend).__name__}')
