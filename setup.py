9b10ef67790263cf9effc807928b106db29455e0
Alain St-Denis <alain@zenfolie.org>
2023-03-12 17:20:16 -0400

    Add funcy to the reqs

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "cryptography",
         "cssselect>=0.9.2",
         "dirty-validators>=0.3.0",
+        "funcy",
         "lxml>=3.4.0",
         "iteration_utilities",
         "jsonschema>=2.0.0",
