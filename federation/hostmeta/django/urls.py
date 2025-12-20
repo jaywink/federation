4d68c6968e233aa1c69abe61be485ab6cfe7d9fa
Alain St-Denis <alain@zenfolie.org>
2025-12-20 10:25:35 -0500

    replace deprecated url method with re_path for django >=4.2.

--- federation/hostmeta/django/urls.py
+++ federation/hostmeta/django/urls.py
@@ -1,5 +1,5 @@
 # noinspection PyPackageRequirements
-from django.conf.urls import url
+from django.urls import re_path
 
 from federation.hostmeta.django import rfc7033_webfinger_view
 from federation.hostmeta.django.generators import (
@@ -7,8 +7,8 @@ from federation.hostmeta.django.generators import (
 )
 
 urlpatterns = [
-    url(r'^.well-known/matrix/client$', matrix_client_wellknown_view, name="matrix-client-wellknown"),
-    url(r'^.well-known/matrix/server$', matrix_server_wellknown_view, name="matrix-server-wellknown"),
-    url(r'^.well-known/webfinger$', rfc7033_webfinger_view, name="rfc7033-webfinger"),
-    url(r'^.well-known/x-nodeinfo2$', nodeinfo2_view, name="nodeinfo2"),
+    re_path(r'^.well-known/matrix/client$', matrix_client_wellknown_view, name="matrix-client-wellknown"),
+    re_path(r'^.well-known/matrix/server$', matrix_server_wellknown_view, name="matrix-server-wellknown"),
+    re_path(r'^.well-known/webfinger$', rfc7033_webfinger_view, name="rfc7033-webfinger"),
+    re_path(r'^.well-known/x-nodeinfo2$', nodeinfo2_view, name="nodeinfo2"),
 ]
