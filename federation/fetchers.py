4c2755796eefc7dea2d63a12b96a555a7016ee9c
Alain St-Denis <alain@zenfolie.org>
2026-02-20 07:59:31 -0500

    add a protocol argument to retrieve_remote_content.

--- federation/fetchers.py
+++ federation/fetchers.py
@@ -6,6 +6,7 @@ from federation import identify_protocol_by_id
 from federation.entities.base import Profile
 from federation.protocols.activitypub import protocol as activitypub_protocol
 from federation.protocols.diaspora import protocol as diaspora_protocol
+from federation.protocols.enums import ProtocolType
 from federation.utils.text import validate_handle
 
 logger = logging.getLogger("federation")
@@ -14,15 +15,16 @@ logger = logging.getLogger("federation")
 def retrieve_remote_content(
         id: str, guid: str = None, handle: str = None, entity_type: str = None,
         sender_key_fetcher: Callable[[str], str] = None, cache: bool=True,
+        protocol: ProtocolType = None
 ):
     """Retrieve remote content and return an Entity object.
 
     ``sender_key_fetcher`` is an optional function to use to fetch sender public key. If not given, network will be used
     to fetch the profile and the key. Function must take federation id as only parameter and return a public key.
     """
-    protocols = (activitypub_protocol, diaspora_protocol)
+    protocols = (ProtocolType.ACTIVITYPUB, ProtocolType.DIASPORA) if protocol == None else (protocol,)
     for protocol in protocols:
-        utils = importlib.import_module(f"federation.utils.{protocol.PROTOCOL_NAME}")
+        utils = importlib.import_module(f"federation.utils.{protocol.string}")
         content = utils.retrieve_and_parse_content(
             id=id, guid=guid, handle=handle, entity_type=entity_type, 
             cache=cache, sender_key_fetcher=sender_key_fetcher,
